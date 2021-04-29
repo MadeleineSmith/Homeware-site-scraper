58c6e3a11825743214acc3f5ce6f67c9bbc1a267
Madeleine Smith <madeleinesmith222@gmail.com>
2021-04-29 12:41:09 +0100

    allow scraping of all product categories

--- ecommerce/ecommerce/spiders/homeware.py
+++ ecommerce/ecommerce/spiders/homeware.py
@@ -8,9 +8,15 @@ class HomewareSpider(scrapy.Spider):
     base_url = 'https://cultartfusion.com'
 
     def start_requests(self):
-        yield scrapy.Request(url='https://cultartfusion.com/collections/furniture/', callback=self.parse_response)
+        yield scrapy.Request(url=self.base_url, callback=self.parse_home_page)
 
-    def parse_response(self, response):
+    def parse_home_page(self, response):
+        product_category_urls = response.xpath('//li[@class="site-nav--has-dropdown "][1]/ul/li/ul/li/a/@href').getall()
+
+        for product_category_url in product_category_urls:
+            yield scrapy.Request(url=urljoin(self.base_url, product_category_url), callback=self.parse_product_category_page)
+
+    def parse_product_category_page(self, response):
         products_urls = response.xpath('//a[@class="product-grid-item"]/@href').extract()
 
         for product_url in products_urls:
@@ -18,7 +24,7 @@ class HomewareSpider(scrapy.Spider):
 
         next_page = response.xpath('//a[@title="Next »"]/@href').extract_first()
         if next_page:
-            yield scrapy.Request(url=urljoin(self.base_url, next_page), callback=self.parse_response)
+            yield scrapy.Request(url=urljoin(self.base_url, next_page), callback=self.parse_product_category_page)
 
     def parse_product_page(self, response):
         yield {
@@ -26,5 +32,5 @@ class HomewareSpider(scrapy.Spider):
             'Description': response.xpath('//div[contains(concat(" ", normalize-space(@class)," "), "product-description")]').get(),
             'Regular Price': response.xpath('//meta[@itemprop="price"]/@content').get(),
             'Category': response.xpath('//nav[@class="breadcrumb"]/a[2]/text()').get(),
-            'Images': response.xpath('//ul[contains(concat(" ", normalize-space(@class)," "), "product-photo-thumbs")]/li/a/@href').getall(),
+            'Images': response.xpath('//ul[contains(concat(" ", normalize-space(@class)," "), "product-photo-thumbs")]/li/a/@href').getall()
         }
